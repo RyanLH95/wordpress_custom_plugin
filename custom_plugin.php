51130cc9a2151de237f426565b15f7ca5c84c4c6
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-14 21:26:45 +0100

    Add a add_action function with 2 arguments of 'admin_men' and an array with a  keyword and an 'adminPage' string. Then create adminPage into a function of it's own

---
+++ custom_plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Custom Plugin
+ * Plugin URI: N/A
+ * Description: Designed to create a new settings page for word counts as well as self updating when making changes.
+ * Version: 1.0
+ * Author: Ryan Henry
+ */
+
+ class WordCountAndTimePlugin {
+    // this function will be the base for pushing content onto the page
+    function __construct() {
+        add_action('admin_menu', array($this, 'admin_page'));
+    }
+ }
+
+ function adminPage() {
+    add_options_page(1, 2, 3, 4, 5);
+ }
+
+ add_filter( 'auto_update_plugin', '__return_true' );
