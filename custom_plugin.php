20575e431e1836744a1bcdd9dd50bb7499fb0c5c
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-15 07:32:07 +0100

    inside the __construct() function, Add another add_action function with the arguments of  and 'settings'. Then create a settings function below

--- custom_plugin.php
+++ custom_plugin.php
@@ -10,8 +10,14 @@
  class WordCountAndTimePlugin {
     // this function will be the base for pushing content onto the page
     function __construct() {
-        add_action('admin_menu', array($this, 'admin_page'));
+        add_action('admin_menu', array($this, 'adminPage'));
+        add_action('admin_init', array($this, 'settings'));
     }
+ }
+
+ // This function is responsible for adding custom setting to database
+ function settings() {
+
  }
 
  function adminPage() { // first argument is for title of page, second is name of settings,
@@ -23,5 +29,8 @@
       <h1>Word Count Settings</h1>
     </div>
  <?php }
+ 
+ // to create a new instance of class above
+ $WordCountAndTimePlugin = new WordCountAndTimePlugin();
 
  add_filter( 'auto_update_plugin', '__return_true' );
