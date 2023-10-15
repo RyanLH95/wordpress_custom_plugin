946080830084c1af15a49a529bbf0a65bb51a22f
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-15 09:12:06 +0100

    Inside <div> in adminPage function, open and close <?php tag and add a settings_fields function containing 'wordcountplugin', a do_settings_section function contain slug for url and a submit_button that saves changes made in wordpress

--- custom_plugin.php
+++ custom_plugin.php
@@ -13,31 +13,40 @@
         add_action('admin_menu', array($this, 'adminPage'));
         add_action('admin_init', array($this, 'settings'));
     }
- }
 
  // This function is responsible for adding custom setting to database
- function settings() {
-   add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_location')); // create 5 arguments
-   register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => array($this, 'sanitizeLocation'), 'default' => '0'));
+    function settings() {
+      add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+
+      add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_location')); // create 5 arguments
+      register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => array($this, 'sanitizeLocation'), 'default' => '0'));
+   }
+
+   function locationHTML() { ?>
+     <select name="wcp_location">
+       <option value="0"<?php selected(get_option('wcp_location'), '0') ?>>Beginning of post</option>
+       <option value="1"<?php selected(get_option('wcp_location'), '1') ?>>End of post</option>
+     </select>
+     <?php }
+
+   function adminPage() {
+     add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'pluginHTML'));
+   }
+   
+   // to include actual HTML for structure of page
+   function pluginHTML() { ?>
+      <div name="wrap">
+        <h1>Word Count Settings</h1>
+        <form action="options.php" method="POST"></form>
+        <?php
+          settings_fields('wordcountplugin');
+          do_settings_selections('word-count-settings-page');
+          submit_button();
+        ?>
+      </div>
+   <?php }
  }
 
- function locationHTML() { ?>
-   <select name="wcp_location">
-     <option value="0"<?php selected(get_option('wcp_location'), '0') ?>>Beginning of post</option>
-     <option value="1"<?php selected(get_option('wcp_location'), '1') ?>>End of post</option>
-   </select>
- <?php }
-
- function adminPage() { // first argument is for title of page, second is name of settings,
-    add_options_page('Word Count Settings', 'Word Count', 'manage options', 'word-count-settings-page', array($this, pluginHTML));
- }
-
- function pluginHTML() { ?>
-    <div name="wrap">
-      <h1>Word Count Settings</h1>
-    </div>
- <?php }
- 
  // to create a new instance of class above
  $WordCountAndTimePlugin = new WordCountAndTimePlugin();
 
